ca1a60df7a40c0de75cc9bcf4531eee6dc2209d0
mohamed-sassi <mohamed.sassi625@gmail.com>
2020-08-13 16:27:46 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <router-view/>
+  </div>
+</template>
+
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+export default {
+  components:{
+    Navbar
+  }
+}
+</script>
+
+<style>
+    *{
+        box-sizing: border-box;
+        padding: 0;
+        margin: 0;
+    }
+</style>
