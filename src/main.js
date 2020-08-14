c3026aa4d0f0e177a0ca5be8a6bea4f5ecb167eb
mohamed sassi <mohamed.sassi625@gmail.com>
2020-08-14 18:19:06 +0200

    test

--- src/main.js
+++ src/main.js
@@ -2,20 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { BootstrapVue } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import VueSidebarMenu from 'vue-sidebar-menu'
-import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
 import vuetify from './plugins/vuetify';
-
-Vue.use(VueSidebarMenu)
-
 
 
 Vue.config.productionTip = false
 
-Vue.use(BootstrapVue)
 new Vue({
   router,
   store,
