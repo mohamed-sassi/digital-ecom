fe8ade895c964e0020d99486ec209cd7af4115ea
mohamed sassi <mohamed.sassi625@gmail.com>
2020-08-16 02:19:12 +0200

    few changes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <v-app id="app">
     <Navbar />
-    <Head/>
     <v-main>
       <v-row>
         <v-col cols="3">
@@ -19,13 +18,11 @@
 <script>
 import Navbar from "@/components/Navbar/Navbar.vue";
 import Sidebar from "@/components/Sidebar/Sidebar.vue";
-import Head from "@/components/HomePage/Head.vue"
 
 export default {
   components: {
     Navbar,
     Sidebar,
-    Head,
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,21 @@
 <template>
   <div class="home">
-   
+    <Head/>
     <v-divider color="grey"></v-divider>
     <div class="pt-5">
       <h1>Trending Assets</h1>
-      <Assets />
+      <Assets/>
     </div>
   </div>
 </template>
 
 <script>
 import Assets from "@/components/Assets/Assets.vue";
-
+import Head from "@/components/HomePage/Head"
 export default {
   components: {
     Assets,
-    
+    Head
   },
   name: "Home",
 };
