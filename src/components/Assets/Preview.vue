584d5eda22a800038d0fb0447541f5c57a3dcab8
mohamed sassi <mohamed.sassi625@gmail.com>
2020-08-20 07:26:18 +0200

    added 3d model preview

--- src/components/Assets/Preview.vue
+++ src/components/Assets/Preview.vue
@@ -1,60 +1,74 @@
 <template>
-    <div id="container"></div>
+  <div id="container">
+    <Loading v-if="loading"/>
+  </div>
 </template>
 
 <script>
-import * as Three from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import Loading from './Loading'
+import * as Three from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 export default {
+  components:{
+    Loading
+  },
   data() {
     return {
       camera: null,
       scene: null,
       renderer: null,
-      controls:null
-    }
+      controls: null,
+      loading:false
+    };
   },
   methods: {
-    init: function() {
-        let container = document.getElementById('container');
+    init() {
+      let container = document.getElementById("container")
+      this.camera = new Three.PerspectiveCamera(70,container.clientWidth / container.clientHeight,0.01,500);
+      this.camera.position.x = 5
 
-        this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight,.01, 500);
-        this.camera.position.x = 10
-        this.camera.position.y = 0
-        this.scene = new Three.Scene();
-        var hlight = new Three.AmbientLight(0x404040,1000);
-        this.scene.add(hlight)
+      this.scene = new Three.Scene();
+      this.scene.background = new Three.Color(0xdddddd)
 
-        this.renderer = new Three.WebGLRenderer({antialias: true});
-        this.renderer.setSize(container.clientWidth, container.clientHeight);
-        container.appendChild(this.renderer.domElement);
+      var ambientLight = new Three.AmbientLight(0x404040, 500);
+      this.scene.add(ambientLight);
 
-        this.controls = new OrbitControls(this.camera,container)
-        var loader = new GLTFLoader()
+      var directionalLight = new Three.DirectionalLight(0xffffff,70)
+      directionalLight.position.set(0,1,0)
+      directionalLight.castShadow = true
+      this.scene.add(directionalLight)
 
-        loader.load('carModel/scene.gltf', model => {
-            // model.scene.children[0].scale.set(0.5,0.5,0.5)
-            model.scene.children[0].rotateZ(90/180 * Math.PI)
-            this.scene.add(model.scene)
-            this.animate()
-            console.log("loaded")
-        })
+      this.renderer = new Three.WebGLRenderer({ antialias: true });
+      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(this.renderer.domElement);
+
+      this.controls = new OrbitControls(this.camera, container);
+      var loader = new GLTFLoader();
+
+      loader.load("carModel/scene.gltf", (model) => {
+        model.scene.children[0].rotateZ((45 / 180) * Math.PI);
+        this.scene.add(model.scene);
+        this.animate();
+        this.loading = false
+      },()=>{
+        this.loading = true
+      });
     },
-    animate: function() {
-        this.renderer.render(this.scene, this.camera);
-        // console.log(this.camera.rotation.y)
-        requestAnimationFrame(this.animate);
-    }
+    animate() {
+      this.renderer.render(this.scene, this.camera);
+      requestAnimationFrame(this.animate);
+    },
   },
   mounted() {
-      this.init();
-  }
-}
+    this.init();
+  },
+};
 </script>
 
 <style scoped>
-#container{
-    height: 600px;
+#container {
+  height: 600px;
+  background-color: #dddddd;
 }
 </style>
